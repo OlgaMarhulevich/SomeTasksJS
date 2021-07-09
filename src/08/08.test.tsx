bd501e61f2f9b78ac65b455f6ab3d373755b94ef
Olga <belaya-o.v@yandex.by>
2021-07-09 17:34:56 +0300

    08

---
+++ src/08/08.test.tsx
@@ -0,0 +1,5 @@
+import {users} from "./08";
+
+test ('users', () => {
+    expect(users.s356.id).toBe(356)
+})
---
+++ src/08/08.tsx
@@ -0,0 +1,7 @@
+export type userType = {
+    [key: string]: {id: number, name: string}
+}
+
+export const users: userType = {
+    's356': {id: 356, name: 'John'}
+}
